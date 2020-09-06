41a3018a372c54b7c900841a37bf7f1c6bd09d35
alex hill <hill.alex.james@gmail.com>
2020-09-06 11:48:38 -0500

    adds homepage and characterclass

---
+++ src/app/classes/dndCharacters/dnd5e-character.spec.ts
@@ -0,0 +1,7 @@
+import { Dnd5eCharacter } from './dnd5e-character';
+
+describe('Dnd5eCharacter', () => {
+  it('should create an instance', () => {
+    expect(new Dnd5eCharacter()).toBeTruthy();
+  });
+});
---
+++ src/app/classes/dndCharacters/dnd5e-character.ts
@@ -0,0 +1,55 @@
+import { Character } from '../character';
+import { NumberSymbol } from '@angular/common';
+
+export class Dnd5eCharacter extends Character {
+
+    //Details
+    alignment:string;
+    background:string;
+
+    //Scores
+    strengthScore:number;
+    dexterityScore:number;
+    constitutionScore:number;
+    intelligenceScore:number;
+    wisdomScore:number;
+    charismaScore:number;
+
+    //Stats
+    hitPointsMax:number;
+    hitPointsCurrent:number;
+    temporaryHitPoints:number;
+    armorClass:number;
+    proficiencyBonus:number;
+    initiativeBonus:number;
+    speed:number;
+
+    //Saves
+    strengthSaveProficient:boolean;
+    dexteritySaveProficient:boolean;
+    constitutionSaveProficient:boolean;
+    intelligenceSaveProficient:boolean;
+    wisdomSaveProficient:boolean;
+    charismaSaveProficient:boolean;
+
+    //Skills
+    acrobaticsProficient:boolean;
+    animalHandlingProficient:boolean;
+    arcanaProficient:boolean;
+    athleticsProficient:boolean;
+    deceptionProficient:boolean;
+    historyProficient:boolean;
+    insightProficient:boolean;
+    intimidationProficient:boolean;
+    investigationProficient:boolean;
+    medicineProficient:boolean;
+    natureProficient:boolean;
+    perceptionProficient:boolean;
+    performanceProficient:boolean;
+    persuasionProficient:boolean;
+    religionProficient:boolean;
+    sleightOfHandProficient:boolean;
+    stealthProficient:boolean;
+    survivalProficient:boolean;
+
+}
